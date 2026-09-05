b3bec43400c12e7c555e1bd0340c71eb995f3b41
shivamrawat94090-eng <shivamrawat94090@gmail.com>
2026-09-05 22:17:03 +0530

    Add files via upload

---
+++ 50.c
@@ -0,0 +1,42 @@
+//Q50: Write a program to print the following pattern:
+/*****
+ /****
+  ***
+   **
+    *
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+
+        for (int j = i; j < 5; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
