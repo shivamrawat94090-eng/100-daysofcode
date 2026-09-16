990cd65ca7e4475ca3260306420d39cd4d82cdf5
shivamrawat94090-eng <shivamrawat94090@gmail.com>
2026-09-16 16:40:29 +0530

    Add files via upload

---
+++ 75.c
@@ -0,0 +1,58 @@
+//Q75: Add two matrices.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+2 2
+5 6
+7 8
+Output 1:
+6 8
+10 12
+
+*/
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[10][10], b[10][10], sum[10][10];
+
+    // Input rows and columns
+    scanf("%d %d", &r, &c);
+
+    // Input first matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    scanf("%d %d", &r, &c);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add matrices
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Print result
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
