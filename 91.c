298d8f81a00f4fbe909297f6ec67680cbf924fd1
shivamrawat94090-eng <shivamrawat94090@gmail.com>
2026-09-24 19:18:52 +0530

    Add files via upload

---
+++ 91.c
@@ -0,0 +1,29 @@
+//Q91: Remove all vowels from a string.
+
+/*
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+           str[i] != 'o' && str[i] != 'u') {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
